d16688b9c6c9c38043b8f613a26be3fb14089988
Jean Carlos Canova Gondorek <jean.gondorek@expressosaomiguel.com.br>
2023-04-25 13:48:14 -0300

    first commit

--- src/main/java/com/devs/sdev/controllers/UserController.java
+++ src/main/java/com/devs/sdev/controllers/UserController.java
@@ -3,10 +3,7 @@ package com.devs.sdev.controllers;
 import com.devs.sdev.entity.User;
 import com.devs.sdev.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +22,11 @@ public class UserController {
     @GetMapping(value = "/{id}")
     public User findById(@PathVariable Long id){
         User result = repository.findById(id).get();
+        return result;
+    }
+    @PostMapping
+    public User insert(@RequestBody User user){
+        User result = repository.save(user);
         return result;
     }
 }
