45025065eda69625bdcd2288ad0c55518f315249
Jean Carlos Canova Gondorek <jean.gondorek@expressosaomiguel.com.br>
2023-05-04 13:19:20 -0300

    update

---
+++ src/main/webapp/main.js
@@ -0,0 +1,30 @@
+function fazGet(url){
+    let request = new XMLHttpRequest()
+    request.open("GET", url, false)
+    request.send()
+    return request.responseText
+}
+
+function criaLinha(bairro){
+    let linha = document.createElement("tr")
+    let tdId = document.createElement("td")
+    let tdNd = document.createElement("td")
+    tdId.innerHTML = bairro.id
+    tdNd.innerHTML = bairro.name
+    linha.appendChild(tdId)
+    linha.appendChild(tdNd)
+
+    return linha
+}
+
+function main(){
+    let data = fazGet("http://localhost:8080/users");
+    let bairro = JSON.parse(data);
+    let tabela = document.getElementById("tabela");
+    bairro.forEach(element => {
+        let linha = criaLinha(element);
+        tabela.appendChild(linha);
+    });
+}
+
+main();
---
+++ src/main/webapp/perid.js
@@ -0,0 +1,22 @@
+function fazGet(url){
+    let request = new XMLHttpRequest()
+    request.open("GET", url, false)
+    request.send()
+    return request.responseText
+}
+
+function perid(){
+    let algo = document.getElementById('id');
+    let id = algo.value;
+    let data = fazGet(`http://localhost:8080/users/${id}`);
+    let bairro = JSON.parse(data);
+    let tabela2 = document.getElementById("tabela2");
+    let linha = document.createElement("tr")
+    let tdId = document.createElement("td")
+    let tdNd = document.createElement("td")
+    tdId.innerHTML = bairro.id
+    tdNd.innerHTML = bairro.name
+    linha.appendChild(tdId)
+    linha.appendChild(tdNd)
+    tabela2.appendChild(linha);
+}
